2fcbaae03f5e72bf8476fe10182f980624672316
Quentinine <gamze.ozakinci@gmail.com>
2026-06-06 11:07:28 +0300

    7. commit

--- src/test/java/Tests/_406_hastaArama.java
+++ src/test/java/Tests/_406_hastaArama.java
@@ -1,4 +1,27 @@
 package Tests;
 
-public class _406_hastaArama {
+import Utilities.Functions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class _406_hastaArama extends Functions {
+
+    @Test(groups = "PatientManagement")
+    public void hastaAramaPos() {
+
+        driver.findElement(By.cssSelector("#coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension"));
+        WebElement findPatient = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#patient-search")));
+        findPatient.sendKeys("da");
+        WebElement firstPatient = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class =\"odd\"][1]")));
+        firstPatient.click();
+
+    }
+    @Test(groups = "PatientManagement")
+    public void hastaAramaNeg() {
+
+
+
+    }
 }
--- src/test/java/Tests/_407_hastaSilme.java
+++ src/test/java/Tests/_407_hastaSilme.java
@@ -1,4 +1,16 @@
 package Tests;
 
-public class _407_hastaSilme {
+import Utilities.Functions;
+import org.testng.annotations.Test;
+
+public class _407_hastaSilme extends Functions {
+
+    @Test(groups = {"PatientManagement","Smoke"})
+    public void hastaSilme() {
+
+
+
+
+    }
+
 }
